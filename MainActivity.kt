d2aa7df2135ea8c04cb2c37bfd4d96fe5749d5bb
shinya0226 <79456274+shinya0226@users.noreply.github.com>
2021-04-14 11:43:45 +0900

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.swipableviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val images = listOf(
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_foreground,
+            R.mipmap.ic_launcher
+
+        )
+
+        val adapter = ViewPagerAdapter(images)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager){tab, position -> tab.text = "Tab ${position+1}"
+        }.attach()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Unselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Reselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
+
+
+
+
+    }
+}
